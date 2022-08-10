63acd230f444018b03ae8901fb3549f6ada761f0
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-08-10 14:00:44 -0700

    Added .c for printing formatted string

---
+++ _printf.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _printf - prints formatted string to stdout
+ * @format: input string of characters
+ * Return: formatted string
+ */
+int _printf(const char *format, ...)
+{
+	int char_to_print;
+	struct_f functs[] = {
+		{"c", char_},
+		{"s", str},
+		{"%", p_cent},
+		{NULL, NULL}
+	};
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	chars_to_print = comp(format, functs, args);
+	va_end(args);
+	return (chars_to_print);
+}
